1b65dd72ba9dcaef2a4254b10978fbceb8a21103
plusmail <plus4957@gmail.com>
2024-08-03 18:02:40 +0900

    devide history

---
+++ src/main/java/com/yi/App.java
@@ -0,0 +1,16 @@
+package com.yi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "M1" );
+        System.out.println( "M2" );
+        System.out.println( "M3" );
+    }
+}
